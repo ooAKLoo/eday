c4bafe886d891ca32e648bfec78c3939af517b7a
ooAKLoo <60376794+ooAKLoo@users.noreply.github.com>
2023-08-04 16:33:21 +1000

    initial upload
    initial upload

---
+++ eDay2_1/CoreData/DocuData+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  DocuData+CoreDataClass.swift
+//  eDay5
+//
+//  Created by 杨东举 on 2022/1/12.
+//
+
+import Foundation
+import CoreData
+
+@objc(Test)
+public class Docu: NSManagedObject {
+
+}
